9f9a4305dbf595ecbd5ce151fcb37d09f12c9003
Cody-Joe Abe <cody.masao@gmail.com>
2019-03-25 11:30:39 -0700

    added a recent project

---
+++ app/components/FoodOrder.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+
+const FoodOrderProjectDiv= styled.div
+`
+
+`
+
+const FoodOrderHeader = styled.h1
+`
+  text-align: left;
+  font-size: 40px;
+  padding-left: 20px;
+`
+
+const FoodOrderBody = styled.div
+`
+  text-align: left;
+  padding-left: 20px;
+  padding-bottom: 20px;
+`
+const FoodOrderDiv = styled.div
+`
+
+`
+
+class FoodOrder extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+
+  render(){
+    return(
+    <FoodOrderProjectDiv>
+      <FoodOrderHeader>
+        Food Order
+      </FoodOrderHeader>
+      <FoodOrderBody>
+        <p> Send your friends an order to make you food. Include special instructions for them. You can email them out of the blue as a prank, or make special arrangements, either way this is a great tool for sending random emails to your friends to think they are being spammed! </p>
+        <br/>
+        <p>Find out more on <a target="_blank" href="https://github.com/codemasa/FoodSelectionWebApp">GitHub</a> or <a target="_blank" href="https://food-order-web-app.herokuapp.com">Demo</a></p>
+
+      </FoodOrderBody>
+    </FoodOrderProjectDiv>
+    )
+  }
+}
+
+export default FoodOrder;
--- app/pages/Projects.jsx
+++ app/pages/Projects.jsx
@@ -5,6 +5,7 @@ import Contents from '../components/Contents.jsx';
 import SSCProject from '../components/SSCProject.jsx';
 import LissajousProject from '../components/Lissajous.jsx';
 import TakeANoteProject from '../components/TakeANote.jsx';
+import FoodOrder from '../components/FoodOrder.jsx';
 
 
 
@@ -14,6 +15,7 @@ const Projects = () => (
       <title>About Page</title>
     </Helmet>
     <Contents>
+      <FoodOrder/>
       <TakeANoteProject/>
       <SSCProject/>
       <LissajousProject/>
