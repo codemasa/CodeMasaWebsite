b635d69acd607c88400dbdd98d9cbf3046be6e0f
Cody-Joe Abe <cody.masao@gmail.com>
2019-04-25 11:58:55 -0700

    re-added a file I accidentally deleted

---
+++ app/components/Form.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(){
+      super();
+      this.state = {
+          name : "",
+          email : "",
+          message : "",
+          formError: false
+      }
+  }
+
+
+  render() {
+
+
+
+    return (
+      <div>
+        Here
+      </div>
+    );
+  }
+}
+
+export default Form;
